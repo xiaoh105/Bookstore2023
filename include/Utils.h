7636bd79703e4c5e36fb6dbeb68d077cce91acf2
xiaoh <xiaoh105@sjtu.edu.cn>
2023-12-05 19:16:16 +0800

    Upd: Finish implementation of InstructionValidator.

--- include/Utils.h
+++ include/Utils.h
@@ -8,4 +8,6 @@ constexpr unsigned long long pow = 1314527;
 
 unsigned long long GetHash(const string &s);
 
+int StrCount(const string &s, const string &pattern);
+
 #endif //HOMEWORK5_BOOKSTORE_UTILS_H
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -7,3 +7,30 @@ unsigned long long GetHash(const string &s)
     ret = ret * pow + i;
   return ret;
 }
+
+unsigned long long CalcHash(const unsigned long long *h,
+                            const unsigned long long *p, int l, int r)
+{
+  if (l == 0) { return h[r]; }
+  else { return h[r] - h[l - 1] * p[r - l + 1]; }
+}
+
+int StrCount(const string &s, const string &pattern)
+{
+  if (pattern.length() > s.length()) return 0;
+  unsigned long long h[s.length() + 5], h_pattern;
+  unsigned long long p[s.length() + 5];
+  h[0] = s[0], h_pattern = pattern[0], p[0] = 1;
+  for (int i = 1; i < s.length(); ++i)
+  {
+    h[i] = h[i - 1] * pow + s[i];
+    p[i] = p[i - 1] * pow;
+  }
+  for (int i = 1; i < pattern.length(); ++i)
+    h_pattern = h_pattern * pow + pattern[i];
+  int ret = 0;
+  for (int i = 0; i + pattern.length() <= s.length(); ++i)
+    if (CalcHash(h, p, i, i + pattern.length() - 1) == h_pattern)
+      ++ret;
+  return ret;
+}
