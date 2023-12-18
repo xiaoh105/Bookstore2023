43466d3fecb23d42ba97b96508b0ece72a5f05a6
xiaoh <xiaoh105@sjtu.edu.cn>
2023-12-18 11:33:25 +0800

    Upd: Remove debugging contents & refactor CMakeLists.txt.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,9 @@
+// Passed! Fuck bookstore!
 #include "Executor.h"
 
 Executor executor;
 
 int main()
 {
-  /*system("rm BookAuthor");
-  system("rm BookCode");
-  system("rm BookInfo");
-  system("rm BookKeyword");
-  system("rm BookName");
-  system("rm UserID");
-  system("rm UserInfo");*/
   executor.listen();
 }
